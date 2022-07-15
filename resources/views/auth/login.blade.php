ad10cc0ad315a30522f5b9a361d4209aacc6eecf
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-16 06:01:31 +0700

    👍 Added: Customization Halaman Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,96 @@
-<x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
+<x-authentication>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card overflow-hidden">
+                    <div class="row g-0">
+                        <div class="col-lg-6">
+                            <div class="p-lg-5 p-4 auth-one-bg h-100">
+                                <div class="bg-overlay"></div>
+                                <div class="position-relative h-100 d-flex flex-column">
+                                    <div class="mb-4">
+                                        <a href="{{route('dashboard')}}" class="d-block">
+                                            <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="18">
+                                        </a>
+                                    </div>
+                                    <div class="mt-auto">
+                                        <div class="mb-3">
+                                            <i class="ri-double-quotes-l display-4 text-success"></i>
+                                        </div>
 
-        <x-jet-validation-errors class="mb-4" />
+                                        <div id="qoutescarouselIndicators" class="carousel slide" data-bs-ride="carousel">
+                                            <div class="carousel-indicators">
+                                                <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                                <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                                <button type="button" data-bs-target="#qoutescarouselIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                            </div>
+                                            <div class="carousel-inner text-center text-white-50 pb-5">
+                                                <div class="carousel-item active">
+                                                    <p class="fs-15 fst-italic">"Ilmu tanpa amal adalah kegilaan, dan amal tanpa ilmu adalah kesia-siaan." - Imam Ghazali </p>
+                                                </div>
+                                                <div class="carousel-item">
+                                                    <p class="fs-15 fst-italic">"Menuntut ilmu (agama) adalah kewajiban bagi setiap muslim." - Hadis</p>
+                                                </div>
+                                                <div class="carousel-item">
+                                                    <p class="fs-15 fst-italic">"Teruslah menjadi orang yang mencari ilmu kapanpun dan di manapun."</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- end carousel -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end col -->
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
+                        <div class="col-lg-6">
+                            <div class="p-lg-5 p-4">
+                                <div>
+                                    <h5 class="text-primary">Selamat Datang Kembali !</h5>
+                                    <p class="text-muted">Silahkan login untuk mendapatkan akses ke Madrosy.</p>
+                                </div>
+
+                                <div class="mt-4">
+                                    <form action="{{route('login')}}" method="POST">
+
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">Username</label>
+                                            <input type="text" name="email" value="{{old('email')}}" class="form-control" id="username" autofocus placeholder="Enter username">
+                                        </div>
+
+                                        <div class="mb-3">
+                                            <div class="float-end">
+                                                <a href="{{route('password.request')}}" class="text-muted">Forgot password?</a>
+                                            </div>
+                                            <label class="form-label" for="password-input">Password</label>
+                                            <div class="position-relative auth-pass-inputgroup mb-3">
+                                                <input name="password" type="password" class="form-control pe-5" placeholder="Enter password" id="password-input">
+                                                <button class="btn btn-link position-absolute end-0 top-0 text-decoration-none text-muted" type="button" id="password-addon"><i class="ri-eye-fill align-middle"></i></button>
+                                            </div>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" value="" id="auth-remember-check">
+                                            <label class="form-check-label" for="auth-remember-check">Remember me</label>
+                                        </div>
+
+                                        <div class="mt-4">
+                                            <button class="btn btn-success w-100" type="submit">Sign In</button>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end col -->
+                    </div>
+                    <!-- end row -->
+                </div>
+                <!-- end card -->
             </div>
-        @endif
+            <!-- end col -->
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
-</x-guest-layout>
+        </div>
+        <!-- end row -->
+    </div>
+</x-authentication>
---
+++ resources/views/layouts/authentication.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg">
+<head>
+
+    <meta charset="utf-8" />
+    <title>Sign In | Aplikasi Management Pondok Pesantren</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <meta content="{{csrf_token()}}" name="csrf-token" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{asset('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
+
+
+</head>
+
+<body>
+
+<!-- auth-page wrapper -->
+<div class="auth-page-wrapper auth-bg-cover py-5 d-flex justify-content-center align-items-center min-vh-100">
+    <div class="bg-overlay"></div>
+    <!-- auth-page content -->
+    <div class="auth-page-content overflow-hidden pt-lg-5">
+        {{$slot}}
+        <!-- end container -->
+    </div>
+    <!-- end auth page content -->
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <p class="mb-0">&copy;
+                            <script>document.write(new Date().getFullYear())</script> Velzon. Crafted with <i class="mdi mdi-heart text-danger"></i> by Themesbrand
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- end Footer -->
+</div>
+<!-- end auth-page-wrapper -->
+
+<!-- JAVASCRIPT -->
+<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+<script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+<script src="{{asset('assets/js/plugins.js')}}"></script>
+
+</body>
+</html>
