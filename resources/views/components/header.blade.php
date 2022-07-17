c31c4552a910d79dcddf9ee068462ad1a461ceb4
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-18 06:36:33 +0700

    😍 Updated: Ganti nama sesuai akun login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -787,17 +787,17 @@
                         <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
                         <span class="d-flex align-items-center">
-                            <img class="rounded-circle header-profile-user" src="{{asset('assets/images/users/avatar-1.jpg')}}"
+                            <img class="rounded-circle header-profile-user" src="{{asset('assets/images/users/avatar-7.jpg')}}"
                                  alt="Header Avatar">
                             <span class="text-start ms-xl-2">
-                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">Anna Adame</span>
-                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Founder</span>
+                                <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{auth()->user()->name}}</span>
+                                <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Admin Yayasan</span>
                             </span>
                         </span>
                         </button>
                         <div class="dropdown-menu dropdown-menu-end">
                             <!-- item-->
-                            <h6 class="dropdown-header">Welcome Anna!</h6>
+                            <h6 class="dropdown-header">Welcome!</h6>
                             <a class="dropdown-item" href="pages-profile.html"><i
                                     class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span
                                     class="align-middle">Profile</span></a>
