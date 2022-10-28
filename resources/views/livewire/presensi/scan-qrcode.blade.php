b0e9e1bf9d78b58e2b1625b74e24d9c6054fece4
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-10-29 05:20:18 +0700

    Test Camera

--- resources/views/livewire/presensi/scan-qrcode.blade.php
+++ resources/views/livewire/presensi/scan-qrcode.blade.php
@@ -1,7 +1,19 @@
 <div>
-    <div id="reader" style="width:500px"></div>
-
+    <div id="reader" style="width:auto"></div>
 </div>
+
+@push('styles')
+    <style>
+        #reader {
+            border: 1px solid white;
+            border-radius: 7px;
+        }
+
+        #reader video {
+            border-radius: 10px;
+        }
+    </style>
+@endpush
 
 @push('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5-qrcode/2.2.4/html5-qrcode.min.js"
@@ -9,13 +21,13 @@
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script>
-        const html5QrCode = new Html5Qrcode("reader");
-        const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-            /* handle success */
-        };
-        const config = { fps: 10, qrbox: { width: 250, height: 250 } };
-        // If you want to prefer back camera
-        html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+        function onScanSuccess(decodedText, decodedResult) {
+            console.log(`Code scanned = ${decodedText}`, decodedResult);
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", {fps: 10, qrbox: 250});
+        html5QrcodeScanner.render(onScanSuccess);
     </script>
 @endpush
 
