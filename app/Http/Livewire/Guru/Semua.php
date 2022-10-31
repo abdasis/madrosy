28fb0034edd890c77d3b6c91b705c2323d861497
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-10-31 21:37:02 +0700

    Improvement UI Semua guru

--- app/Http/Livewire/Guru/Semua.php
+++ app/Http/Livewire/Guru/Semua.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire\Guru;
 
+use App\Models\Kepegawaian\Guru;
 use Livewire\Component;
 
 class Semua extends Component
 {
     public function render()
     {
-        return view('livewire.guru.semua');
+        return view('livewire.guru.semua', [
+            'total_putra' => Guru::where('jenis_kelamin', 'Laki-Laki')->count(),
+            'total_putri' => Guru::where('jenis_kelamin', 'Perempuan')->count(),
+            'total_guru' => Guru::count(),
+        ]);
     }
 }
