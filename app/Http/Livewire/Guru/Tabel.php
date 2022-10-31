c8e2c6624251ec4395643a71f8ead50c8edbc1ac
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-10-31 21:25:01 +0700

    Penambahan filter pada tabel guru

--- app/Http/Livewire/Guru/Tabel.php
+++ app/Http/Livewire/Guru/Tabel.php
@@ -2,27 +2,65 @@
 
 namespace App\Http\Livewire\Guru;
 
+use App\Models\Akademik\Kelas;
 use App\Models\Kepegawaian\Guru;
 use App\Traits\KonfirmasiHapus;
+use Illuminate\Database\Eloquent\Builder;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use Rappasoft\LaravelLivewireTables\Views\Columns\BooleanColumn;
+use Rappasoft\LaravelLivewireTables\Views\Filters\SelectFilter;
 
 class Tabel extends DataTableComponent
 {
     use KonfirmasiHapus;
+
     protected $model = Guru::class;
 
     protected $listeners = ['hapus' => 'dihapus'];
 
+    public function filters(): array
+    {
+        return [
+            SelectFilter::make('Jenis Kelamin')
+                ->options([
+                    '' => 'Semua',
+                    'Perempuan' => 'Perempuan',
+                    'Laki-Laki' => 'Laki-Laki',
+                ])->filter(function (Builder $query, $value) {
+                    if ($value) {
+                        $query->where('jenis_kelamin', $value);
+                    }
+                }),
+
+            SelectFilter::make('Status')
+                ->options([
+                    '' => 'Pilih Status',
+                    'Tetap' => 'Tetap',
+                    'Kontrak' => 'Kontrak'
+                ])->filter(function (Builder $query, string $status) {
+                    return $query->where('status_guru', $status);
+                }),
+            SelectFilter::make('Jabatan')
+                ->options([
+                    '' => "Pilih Jabatan",
+                    'Kepala Sekolah' => 'Kepala Sekolah',
+                    'Guru' => 'Guru',
+                    'Wali Kelas' => 'Wali Kelas',
+                ])->filter(function (Builder $query, string $status) {
+                    return $query->where('jabatan', $status);
+                })
+        ];
+    }
+
     public function dihapus()
     {
-        if ($this->model_id){
+        if ($this->model_id) {
             $guru = Guru::find($this->model_id)->delete();
 
-            if ($guru){
+            if ($guru) {
                 $this->alert('success', 'Data berhasil dihapus');
-            }else{
+            } else {
                 $this->alert('error', 'Data gagal dihapus');
             }
         }
@@ -31,9 +69,9 @@ class Tabel extends DataTableComponent
     public function configure(): void
     {
         $this->setPrimaryKey('id')
-        ->setTableRowUrl(function ($model) {
-            return route('guru.detail', $model->id);
-        });
+            ->setTableRowUrl(function ($model) {
+                return route('guru.detail', $model->id);
+            });
 
         $this->setDefaultReorderSort('order', 'desc');
 
@@ -44,21 +82,19 @@ class Tabel extends DataTableComponent
         return [
             Column::make('id')->sortable(),
             Column::make("NIK", "nik")
+                ->searchable()
                 ->sortable(),
             Column::make('Nama Lengkap', 'nama')
                 ->sortable()
                 ->searchable(),
             Column::make('Jabatan', 'jabatan')
-                ->sortable()
-                ->searchable(),
+                ->sortable(),
             Column::make('Status', 'status_guru')
-                ->sortable()
-                ->searchable(),
+                ->sortable(),
             Column::make('No. HP', 'no_hp')
-                ->sortable()
-                ->searchable(),
-            BooleanColumn::make('Active', 'status'),
-            Column::make('Opsi', 'id')->format(function ($id, $model, $baris){
+                ->sortable(),
+            BooleanColumn::make('Aktif', 'status'),
+            Column::make('Opsi', 'id')->format(function ($id, $model, $baris) {
                 return view('tombol-aksi', [
                     'hapus' => $id,
                     'edit' => route('guru.edit', $model)
