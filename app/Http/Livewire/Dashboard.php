7106b23f885cc0a624f4e63a1a07815e692c7685
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-11-05 19:19:33 +0700

    Redesign halaman detail santri

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -8,13 +8,7 @@ class Dashboard extends Component
 {
     public function render()
     {
-        $user = auth()->user();
-        $santri = $user->santri;
-        $kelas = $santri->kelas;
-        return view('livewire.dashboard',[
-            'user' => $user,
-            'santri' => $santri,
-            'kelas' => $kelas,
-        ]);
+
+        return view('livewire.dashboard');
     }
 }
