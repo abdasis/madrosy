833157760efeec04b155730b8417f2c2aab99c61
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-11-23 06:02:03 +0700

    Fix kesalahan pengambilan tanggal jatuh tempo

--- app/Services/PaymentGateway/CreateTokenService.php
+++ app/Services/PaymentGateway/CreateTokenService.php
@@ -16,22 +16,25 @@ class CreateTokenService extends Midtrans
 
     public function generateSnapToken()
     {
-        $params = [
-            'transaction_details' => [
-                'order_id' => $this->transaksi->order_id,
-                'gross_amount' => $this->transaksi->total,
-            ],
 
-            'customer_details' => [
-                'first_name' => $this->transaksi->tagihan->santri->nama_lengkap,
-                'last_name' => '',
-                'email' => $this->transaksi->tagihan->santri->email,
-                'phone' => $this->transaksi->tagihan->santri->no_hp,
-            ],
-        ];
 
         try {
+            $params = [
+                'transaction_details' => [
+                    'order_id' => $this->transaksi->order_id,
+                    'gross_amount' => $this->transaksi->total,
+                ],
+
+                'customer_details' => [
+                    'first_name' => $this->transaksi->tagihan->santri->nama_lengkap,
+                    'last_name' => '',
+                    'email' => $this->transaksi->tagihan->santri->email,
+                    'phone' => $this->transaksi->tagihan->santri->no_hp,
+                ],
+            ];
+
             $token =  Snap::getSnapToken($params);
+
             return $token;
         }catch (\Exception $e){
             report($e);
